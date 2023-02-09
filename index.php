9dc31515d7e5feb44ae3f87a33f0c6639cd9422b
davidecardelli <davide.cardelli.94@gmail.com>
2023-02-09 09:45:48 +0100

    add mb_strlen

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 function generatePassword($length) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!?()[]{}+-=/*@#$%^&0*€_';
-    $charactersLength = strlen($characters);
+    $charactersLength = mb_strlen($characters);
     $password = '';
     for ($i = 0; $i < $length; $i++) {
         $password .= $characters[rand(0, $charactersLength - 1)];
